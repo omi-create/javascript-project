6217dc3d8cd007af8b2c5a161379709f6a469c89
omi-create <omitribhuwan@gmail.com>
2026-03-23 15:14:09 +0000

    variables

---
+++ .devcontainer/01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountid = 112233
+let accountemail = "om@gmail.com"
+var accountpassword = "1234"
+accountcity = "Sambhajinagar"
+let accountstate;
+
+// accountid = 2 // not allowed
+
+accountemail = "hc@gmail.com"
+accountpassword = "2134"
+accountcity = "Jaipur"
+
+console.log(accountid);
+console.table([accountid, accountemail, accountpassword, accountcity, accountstate])
+
+/*
+Refer let don't use var
+because fo issue in block scope and functional scope
+*/
+
