1eeff478c0bfe26c2c8c315dbcb2aa17a4ed910a
omi-create <omitribhuwan@gmail.com>
2026-03-23 15:12:27 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node.js not browser
+
+console.log(3 + 3);
+
+let name = "om";
+let age = 19;
+let isLoggedin = false;
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+object 
+*/
+
+
+// console.log(typeof null); null is an object
+// console.log(typeof undefined); undefined
