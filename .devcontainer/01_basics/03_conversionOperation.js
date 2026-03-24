ed6071b234237abae6addd234a31ae6436a91822
omi-create <omitribhuwan@gmail.com>
2026-03-24 15:20:14 +0000

    conversions

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,8 +24,51 @@ let score = "om";
 
 let somenumber = 33
 let stringnumber = String(somenumber)
-console.log(stringnumber);
-console.log(typeof(stringnumber));
+// console.log(stringnumber);
+// console.log(typeof(stringnumber));
+
+// *********************** Operations *****************
+
+let value = 3
+let negval = -value;
+// console.log(negval);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+
+let str1 = "helo";
+let str2 = "hitesh";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;
+// console.log(gameCounter);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
 
 
 
