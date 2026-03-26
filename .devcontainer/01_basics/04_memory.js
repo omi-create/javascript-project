8f8becf82974dab748c2c90510da57e8d1f2bd0e
omi-create <omitribhuwan@gmail.com>
2026-03-26 09:04:26 +0000

    memory

---
+++ .devcontainer/01_basics/04_memory.js
@@ -0,0 +1,27 @@
+// Stack (Primitive), Heap (Non-Premitive)
+// Stack (primitive), Heap (Non-primative)
+let myname = "omtribhuwan"
+let anothername = myname
+anothername = "chaiaurcode";
+
+console.log(myname);
+console.log(anothername);
+
+// Object
+let user1 = {
+    email: "user@gmail.com",
+    upi: "user@ybl",
+}
+
+let user2 = user1;
+
+user2.email = "user1@gmail.com"
+
+// console.log(user1.email);
+// console.log(user2.email);
+name = "om";
+repocount = 30
+
+console.log(`Hello my name is $(name) and my repo count is $(repocount)`);
+
+const gameName = new String(`hitesh`)
