39c0bc466500cb68b0910ff8b3727fa8a1cd8486
omi-create <omitribhuwan@gmail.com>
2026-03-30 14:27:14 +0000

    one

---
+++ .devcontainer/03_basics/06_controlflow/one.js
@@ -0,0 +1,19 @@
+const score = 200
+
+if(score>100){
+    const power="fly"
+    console.log(`User power: ${power}`);
+}
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 10
+val1 = null ?? 10 ?? 20
+// console.log(val1);
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
+
+
+
