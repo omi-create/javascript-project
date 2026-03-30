7797e00b9222340f95209e70dba2f3ab9ab3ead0
omi-create <omitribhuwan@gmail.com>
2026-03-30 13:41:45 +0000

    execution

---
+++ .devcontainer/03_basics/05_execution.js
@@ -0,0 +1,13 @@
+// Javascript Execution Context
+
+// Global EC
+// Global Execution Context
+// Function Execution Context
+// Eval Execution Context
+
+// {} -> memory creation phase
+// Execution Phase
+
+// Global Execution -> this
+// Memory Phase
+
